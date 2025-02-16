4e5f6397c5098bf92bc8d27163feae3744933497
mementojunior123 <presumercj3@gmail.com>
2025-02-16 10:58:44 -0500

    Update menu.py

--- core/menu.py
+++ core/menu.py
@@ -277,38 +277,11 @@ class Menu(BaseMenu):
         BaseUiElements.new_button('BlueButton', 'Test', 1, 'bottomright', (wx - 15, window_size[1] - 15), (0.5, 1.4), 
         {'name' : 'test_button'}, (Menu.font_40, 'Black', False))], #stage 1
 
-        [BaseUiElements.new_button('BlueButton', 'Prev', 1, 'bottomleft', (20, window_size[1] - 25), (0.4, 1.0), 
-        {'name' : 'prev_button'}, (Menu.font_40, 'Black', False)),
-        BaseUiElements.new_button('BlueButton', 'Next', 2, 'bottomright', (wx - 20, window_size[1] - 25), (0.4, 1.0), 
-        {'name' : 'next_button'}, (Menu.font_40, 'Black', False)),
-        BaseUiElements.new_button('BlueButton', 'Back', 3, 'topleft', (15, 15), (0.4, 1.0), 
-        {'name' : 'back_button'}, (Menu.font_40, 'Black', False)),]
+        []
         ]
         self.bg_color = (94, 129, 162)
         self.add_connections()   
 
-    def enter_stage2(self):
-        self.stage = 2
-        sep : int = 4
-        self.stage_data[2]['current_page'] = 0
-        self.stage_data[2]['max_pages'] = ceil(len(test_list) / sep)
-        self.stages[2].append(TestUiGroup.new_group(0))
-    
-    def change_page_stage2(self, new_page : int):
-        self.stage_data[2]['current_page'] = new_page
-        self.find_and_replace(TestUiGroup.new_group(new_page), 2, name='TestGroup')
-    
-    def increment_page_stage2(self):
-        new_page : int = (self.stage_data[2]['current_page'] + 1) % self.stage_data[2]['max_pages']
-        self.change_page_stage2(new_page)
-
-    def decrement_page_stage2(self):
-        new_page : int = (self.stage_data[2]['current_page'] - 1) % self.stage_data[2]['max_pages']
-        self.change_page_stage2(new_page)
-    
-    def exit_stage2(self):
-        self.stage_data[2].clear()
-        self.remove_sprite(2, name='TestGroup')
     
     def update(self, delta : float):
         stage_data = self.stage_data[self.stage]
@@ -329,10 +302,3 @@ class Menu(BaseMenu):
                     pygame.event.post(pygame.Event(core_object.START_GAME, {'mode' : 'test'}))
                 if name == 'test_button':
                     self.goto_stage(2)
-            case 2:
-                if name == 'back_button':
-                    self.goto_stage(1)
-                elif name == 'prev_button':
-                    self.decrement_page_stage2()
-                elif name == 'next_button':
-                    self.increment_page_stage2()
