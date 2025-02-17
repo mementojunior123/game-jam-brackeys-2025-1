9f73dc19af5ddba3432256513cd7ab915d7a81f3
mementojunior123 <presumercj3@gmail.com>
2025-02-17 17:18:25 -0500

    Added a game title

--- core/menu.py
+++ core/menu.py
@@ -271,7 +271,7 @@ class Menu(BaseMenu):
         
         self.stage_data : list[dict] = [None, {}, {}]
         self.stages = [None, 
-        [BaseUiElements.new_text_sprite('Game Title', (Menu.font_60, 'Black', False), 0, 'midtop', (centerx, 50)),
+        [BaseUiElements.new_text_sprite('Dont do it!', (Menu.font_60, 'Black', False), 0, 'midtop', (centerx, 50)),
         BaseUiElements.new_button('BlueButton', 'Play', 1, 'midbottom', (centerx, window_size[1] - 15), (0.5, 1.4), 
         {'name' : 'play_button'}, (Menu.font_40, 'Black', False)),
         BaseUiElements.new_button('BlueButton', 'Test', 1, 'bottomright', (wx - 15, window_size[1] - 15), (0.5, 1.4), 
